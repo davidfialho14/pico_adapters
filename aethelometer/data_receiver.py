a789439d735babbea8c1485f87a9343115b0f55b
david <fialho.david@gmail.com>
2016-08-06 21:48:16 +0100

    implemented the data receiver

--- aethelometer/data_receiver.py
+++ aethelometer/data_receiver.py
@@ -1,4 +1,5 @@
 import socket as sock
+from time import sleep
 
 from aethelometer.data_handler import DataHandler
 
@@ -13,6 +14,10 @@ class DataReceiver:
     def __init__(self, sender_address):
         self._sender_address = sender_address
         self._data_handlers = []
+
+        # stores the data that may have been transferred during a receive call
+        # and did not belong to the current line
+        self._cached_data = ""
 
     def register_data_handler(self, data_handler: DataHandler):
         """
@@ -35,14 +40,75 @@ class DataReceiver:
         chunk of data is received it calls the registered handlers'
         on_new_data() method.
         """
-        pass
+
+        while True:  # allow reconnecting on error
+            try:
+                print("Trying to connect to sender...")
+                with sock.create_connection(self._sender_address) as connection:
+                    print("Connected successfully")
+
+                    while True:
+                        data = self._receive(connection)
+
+                        for handler in self._data_handlers:
+                            handler.on_new_data(data)
+
+            except (ConnectionAbortedError, sock.timeout):
+                print("connection timed out")
+            except (sock.herror, sock.gaierror):
+                print("the address of the aethelometer is not valid")
+                break  # leave the function
+            except (ConnectionRefusedError, sock.error):
+                print("can not reach the aethelometer")
+
+            # retry in connecting in 10 seconds
+            print("connection failed: will try to connect in 10 seconds")
+            sleep(10)
 
     def _receive(self, sender_connection) -> str:
         """
         Blocks until a new chunk of data is received or the connection with the
         sender fails. Before calling this method there must be already a valid
-        connection with the sender.
+        connection with the sender. Raises a socket.timeout if it does not
+        receive any message from the server in 6 minutes or if after receiving
+        data it does not receive more after 1 minute.
         :param sender_connection: socket connection with the sender.
         :return: data received.
         """
-        pass
+        # set a receive timeout of 6 minutes
+        sender_connection.settimeout(6 * 60)
+
+        # get the data cached in the previous receive call
+        data = self._cached_data
+        self._cached_data = ""  # ensure the cached data is cleared
+
+        while True:
+            # the verification of end of data must be the first step of the loop
+            # this is because the cached data ma contain a complete line already
+
+            end_index = data.find("\r\n")
+            if end_index != -1:
+                # reached the end of the data
+
+                # cache extra data
+                self._cached_data = data[end_index + 2:]
+
+                # exclude extra data from the returned data
+                data = data[0:end_index]
+                break
+
+            buffer = sender_connection.recv(512)
+
+            if not buffer:
+                raise ConnectionAbortedError("connection with sender was "
+                                             "correctly closed")
+
+            if not data:  # check if this is the first data chunk
+                # after receiving data chunk set a timeout of 1 minute
+                # this timeout prevents errors due to the server not finishing
+                # the transmission
+                sender_connection.settimeout(1 * 60)
+
+            data += buffer.decode('utf-8')
+
+        return data
