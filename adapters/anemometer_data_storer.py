887f87899607141739e6ca274c3646f999fc7c8d
david <fialho.david@gmail.com>
2016-08-07 17:56:51 +0100

    refactored the data storer to avoid duplicate code

--- adapters/anemometer_data_storer.py
+++ adapters/anemometer_data_storer.py
@@ -1,9 +1,4 @@
-import os
-import shutil
-from datetime import datetime
-
 from data_storer import DataStorer
-from logger import Logging
 
 
 class AnemometerDataStorer(DataStorer):
@@ -14,33 +9,10 @@ class AnemometerDataStorer(DataStorer):
     a new file is created.
     """
 
-    def __init__(self, data_dir, backup_dir):
-        """
-        :param data_dir: directory where to store the data.
-        """
-        self.data_dir = data_dir
-        self.backup_dir = backup_dir
+    def _adjust_data(self, data: str, current_datetime) -> str:
+        """ Adds the date and time to the data line. """
+        return current_datetime.strftime("%d-%m-%Y,%H:%M:%S,") + data
 
-    def store(self, data: str):
-        """ Stores the data inside the configured directory. """
-        current_datetime = datetime.now()
-
-        # include the data time in the data line
-        data = current_datetime.strftime("%d-%m-%Y,%H:%M:%S,") + data
-
-        # filename has the format 3Dddmmyy.CSV
-        out_filename = "3D" + current_datetime.strftime("%d%m%y") + ".CSV"
-        out_filepath = os.path.join(self.data_dir, out_filename)
-
-        if not os.path.exists(out_filepath):
-            # new file will be created
-            # move all current files inside the store dir to the backup dir
-            for filename in os.listdir(self.data_dir):
-                shutil.move(src=os.path.join(self.data_dir, filename),
-                            dst=os.path.join(self.backup_dir, filename))
-                Logging.info("moved file %s to the backup "
-                             "directory" % filename)
-
-        with open(out_filepath, "a") as out_file:
-            out_file.write(data)
-            out_file.write('\n')
+    def _generate_filename(self, data: str, current_datetime) -> str:
+        """ Generates filename with the format 3Dddmmyy.CSV"""
+        return current_datetime.strftime("3D%d%m%y.CSV")
