ac1ae6c38fd7bd1ddf617bc5d0c15677abe2113f
david <fialho.david@gmail.com>
2016-08-08 18:34:19 +0100

    defined the concept of connection handlers

---
+++ adapters/connection_handler.py
@@ -0,0 +1,13 @@
+class ConnectionHandler:
+    """
+    Connection handlers are associated with data receivers and are notified
+    every time the receiver connects or disconnects with the sender (currently
+    disconnection is not supported).
+    """
+
+    def on_new_connection(self, sender_address):
+        """
+        Invoked by the receiver connects with the sender. Takes the address
+        of the server in the standard 2-tuple format.
+        """
+        pass
