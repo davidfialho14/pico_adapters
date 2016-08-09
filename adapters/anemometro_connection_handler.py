8e4a8bcc88f791e143d8fd1edb8d5be1bbd9cdd7
david <fialho.david@gmail.com>
2016-08-09 19:56:43 +0100

    reimplemented the connection handlers to avoid duplicate code
    moved the deploy_cmd() method to the connection handler
    answers are now logged with tag CONFIGURATION

--- adapters/anemometro_connection_handler.py
+++ adapters/anemometro_connection_handler.py
@@ -1,8 +1,4 @@
-import socket
-from datetime import datetime
-
 from connection_handler import ConnectionHandler
-from logger import Logging
 
 
 class AnemometerConnectionHandler(ConnectionHandler):
@@ -30,28 +26,3 @@ class AnemometerConnectionHandler(ConnectionHandler):
             # close command mode
             self._deploy_cmd(data_receiver, connection, "Q")
 
-    def _deploy_cmd(self, data_receiver, connection, cmd):
-        """
-        After sending the command it waits 1 second for answers and
-        ignores them.
-        """
-        # enter in command mode
-        self._send_cmd(connection, cmd)
-        # expect undefined number of answers
-        self._receive_and_log_answer(data_receiver, connection)
-
-    @staticmethod
-    def _receive_and_log_answer(data_receiver, connection):
-        """
-        Waits one second for answers and returns after waiting 1 second
-        with no responses. Dumps the answers to the log file with a tag of
-        'CONFIGURATION'.
-        """
-        try:
-            while True:
-                Logging.info("CONFIGURATION:" +
-                             data_receiver.raw_receive(
-                                 connection, timeout=1).decode())
-
-        except socket.timeout:
-            pass
