0938a7da0751724d825a489774306d0aafead7d3
david <fialho.david@gmail.com>
2016-08-09 11:01:36 +0100

    changed the aethalometer and anemometer scripts to take advantage of the application class

--- adapters/aethalometer.py
+++ adapters/aethalometer.py
@@ -9,53 +9,21 @@ Options:
   --version     Show version.
 
 """
-import os
-import signal
-import sys
-
 from docopt import docopt
 
 from aethalometer_data_storer import AethalometerDataStorer
 from aethalometer_decoder import AethalometerDecoder
+from app import Application
 from configs.aethalometer import AethalometerConfiguration
-from configs.base import LoadError
-from data_receiver import DataReceiver
-from logger import Logging
-
-
-def raise_keyboard_interrupt(signum, frame):
-    raise KeyboardInterrupt
 
 
 def main():
-    args = docopt(__doc__)
+    app = Application(args=docopt(__doc__))
+    config = app.load_config(AethalometerConfiguration)
 
-    try:
-        config = AethalometerConfiguration(args['<config_file>'])
-        config.load()
-    except LoadError as error:
-        print(str(error))
-        sys.exit(1)
-
-    # make terminate signals raise keyboard interrupts
-    signal.signal(signal.SIGTERM, raise_keyboard_interrupt)
-
-    receiver = DataReceiver((config.ip_address, config.port),
-                            AethalometerDecoder)
-    receiver.register_data_handler(AethalometerDataStorer(config.store_dir,
-                                                          config.backup_dir))
-
-    try:
-        Logging.info("Started")
-        receiver.receive_forever()
-    except KeyboardInterrupt:
-        # user pressed Ctrl-C to close the program
-        Logging.info("Closing as requested by user...")
-    except BaseException:
-        Logging.exception("Program will closed due to unexpected error...")
-
-    Logging.info("Closed")
-
+    app.run(decoder=AethalometerDecoder,
+            data_handlers=(AethalometerDataStorer(config.store_dir,
+                                                  config.backup_dir),))
 
 if __name__ == '__main__':
     main()
--- adapters/anemometer.py
+++ adapters/anemometer.py
@@ -9,53 +9,21 @@ Options:
   --version     Show version.
 
 """
-import os
-import signal
-import sys
-
 from docopt import docopt
 
 from anemometer_data_storer import AnemometerDataStorer
 from anemometer_decoder import AnemometerDecoder
+from app import Application
 from configs.anemometer import AnemometerConfiguration
-from configs.base import LoadError
-from data_receiver import DataReceiver
-from logger import Logging
-
-
-def raise_keyboard_interrupt(signum, frame):
-    raise KeyboardInterrupt
 
 
 def main():
-    args = docopt(__doc__)
+    app = Application(args=docopt(__doc__))
+    config = app.load_config(AnemometerConfiguration)
 
-    try:
-        config = AnemometerConfiguration(args['<config_file>'])
-        config.load()
-    except LoadError as error:
-        print(str(error))
-        sys.exit(1)
-
-    # make terminate signals raise keyboard interrupts
-    signal.signal(signal.SIGTERM, raise_keyboard_interrupt)
-
-    receiver = DataReceiver((config.ip_address, config.port),
-                            AnemometerDecoder)
-    receiver.register_data_handler(AnemometerDataStorer(config.store_dir,
-                                                        config.backup_dir))
-
-    try:
-        Logging.info("Started")
-        receiver.receive_forever()
-    except KeyboardInterrupt:
-        # user pressed Ctrl-C to close the program
-        Logging.info("Closing as requested by user...")
-    except BaseException:
-        Logging.exception("Program will closed due to unexpected error...")
-
-    Logging.info("Closed")
-
+    app.run(decoder=AnemometerDecoder,
+            data_handlers=(AnemometerDataStorer(config.store_dir,
+                                                config.backup_dir),))
 
 if __name__ == '__main__':
     main()
