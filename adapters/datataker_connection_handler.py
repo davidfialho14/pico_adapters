b117ab8e0b5b037ebac6331ac5e747e0c6f20579
david <fialho.david@gmail.com>
2016-08-09 19:02:17 +0100

    fixed datataker's sending issue
    the connection handler of the datataker now waits for the commands responses

--- adapters/datataker_connection_handler.py
+++ adapters/datataker_connection_handler.py
@@ -18,11 +18,35 @@ class DataTakerConnectionHandler(ConnectionHandler):
         commands in the cmd_file sends the Time command.
         """
         time_cmd = datetime.now().strftime("T=%H:%M:%S")
-        self._send_cmd(connection, time_cmd)
+        self._deploy_cmd(data_receiver, connection, time_cmd)
 
         date_cmd = datetime.now().strftime("D=%d/%m/%Y")
-        self._send_cmd(connection, date_cmd)
+        self._deploy_cmd(data_receiver, connection, date_cmd)
 
         with open(self._cmd_file) as cmd_file:
             for cmd in cmd_file:
-                self._send_cmd(connection, cmd)
+                self._deploy_cmd(data_receiver, connection,
+                                 cmd.replace("\n", ""))
+
+    def _deploy_cmd(self, data_receiver, connection, cmd):
+        """
+        After sending the command it waits 1 second for answers and
+        ignores them.
+        """
+        try:
+            self._send_cmd(connection, cmd)
+
+            while True:
+                if cmd == "RESET":
+                    # handle the special case of the RESET command
+                    # ignore everything until receiving the done response
+                    # then ignore an extra empty line
+                    answer = b''
+                    while answer != b'Initializing...Done.':
+                        answer = data_receiver.raw_receive(connection, timeout=1)
+                    data_receiver.raw_receive(connection, timeout=1)
+                    break
+                else:
+                    data_receiver.raw_receive(connection, timeout=1)
+        except socket.timeout:
+            pass
